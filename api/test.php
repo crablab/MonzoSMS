fa2356bb4423de164d35f84bac4b830737fa88ed
Hugh <admin@crablab.co.uk>
2016-10-29 22:34:29 +0100

    Moved parser

--- api/test.php
+++ api/test.php
@@ -3,6 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require("../sitetools.php");
+//get the body
 $data = file_get_contents('php://input');
 
 $sendgrid = new SendGrid('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');
