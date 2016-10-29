8141328020bd1ed28f63709f7c0679734cc7c525
Hugh <admin@crablab.co.uk>
2016-10-29 20:43:56 +0100

    test

--- api/test.php
+++ api/test.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $data = "kljhsdlsadlkjsad";
 
 $this->sendgrid = new SendGrid('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');
