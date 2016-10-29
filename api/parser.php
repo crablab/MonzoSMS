1465b1fba0ca4677ef8e2719ed81cef7208990c3
Hugh <admin@crablab.co.uk>
2016-10-29 22:49:59 +0100

    Debug

--- api/parser.php
+++ api/parser.php
@@ -27,7 +27,7 @@ if($test['type'] == "transaction.created"){
     $rec = $db->prepare("SELECT * FROM `users` WHERE `monzo_id` = :id");
     $rec->bindParam(":id", $id);
     $rec->execute();
-
+    print_r($rec->errorInfo());
     $phone = $rec->fetch(PDO::FETCH_ASSOC)['Phone_Number'];
     echo $phone; 
 }
