b60146e7a4720444a40fe17c9d891a8ff1b683c7
Hugh <admin@crablab.co.uk>
2016-10-29 22:43:17 +0100

    Added parse lookup

--- api/parser.php
+++ api/parser.php
@@ -1,47 +1,35 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require("../sitetools.php");
+$sitetools = new sitetools();
+$db = $sitetools->connect();
 /**
  * Created by PhpStorm.
  * User: Vadim
  * Date: 29/10/2016
  * Time: 20:42
  */
-$json = '{
-    "type": "transaction.created",
-    "data": {
-        "account_id": "acc_00008gju41AHyfLUzBUk8A",
-        "amount": -350,
-        "created": "2015-09-04T14:28:40Z",
-        "currency": "GBP",
-        "description": "Ozone Coffee Roasters",
-        "id": "tx_00008zjky19HyFLAzlUk7t",
-        "category": "eating_out",
-        "is_load": false,
-        "settled": true,
-        "merchant": {
-            "address": {
-                "address": "98 Southgate Road",
-                "city": "London",
-                "country": "GB",
-                "latitude": 51.54151,
-                "longitude": -0.08482400000002599,
-                "postcode": "N1 3JD",
-                "region": "Greater London"
-            },
-            "created": "2015-08-22T12:20:18Z",
-            "group_id": "grp_00008zIcpbBOaAr7TTP3sv",
-            "id": "merch_00008zIcpbAKe8shBxXUtl",
-            "logo": "https://pbs.twimg.com/profile_images/527043602623389696/68_SgUWJ.jpeg",
-            "emoji": "🍞",
-            "name": "The De Beauvoir Deli Co.",
-            "category": "eating_out"
-        }
-    }
-}';
+//get the body
+$json = file_get_contents('php://input');
+
 function parse($json){
     $array = json_decode($json, true);
     $result = "You have spent " . abs($array['data']['amount']) . $array['data']['currency'] . " at " . $array['data']['merchant']['address']['address'] . " , " . $array['data']['merchant']['address']['city'] . ", " . $array['data']['merchant']['address']['country'] . '🍞';
-    print_r($result);
+    return $result;
 }
-parse($json);
+
+if($json['type'] == "transaction.created"){
+    $out = parse($json);
+    $id = $json['data']['account_id'];
+    $rec = $db->prepare("SELECT * FROM `users` WHERE `monzo_id` = :id");
+    $rec->bindParam(":id", $id);
+    $rec->execute();
+
+    $phone = $rec->fetch(PDO::FETCH_ASSOC)['Phone_Number'];
+    echo $phone; 
+}
+
 
 ?>
