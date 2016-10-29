e983e9817173f9c3f8325323ad7f2ae1cc2a0a7f
Joe Cibicek <Joe@Jozefs-MacBook-Pro.local>
2016-10-29 19:48:24 +0100

    test

--- index.php
+++ index.php
@@ -1,14 +1,51 @@
+
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 	<title>Monzo SMS</title>
+<style>
+body { 
+background-color: grey;
+}
+
+	#wrapper {    
+    margin: 0 auto;
+    width:1200px;
+    height: 1150px;
+    background-color:#f2f2f2;
+    position: relative;
+}
+</style>
 </head>
 <body>
-	<form method="post" action="api/register.php">
-		<input name="name" type="text" placeholder="Your name" required />
-		<input name="phone" type="phone" placeholder="Your phone number and country code ie. +44xxxxxxx" required />
-		<input name="email" type="email" placeholder="The email you use to login to Monzo" required />
-		<input type="submit" value="Sign up and login to Monzo" />
-	</form>
+	<div id="wrapper">
+
+<div class="container">
+<h4>This will be a fancy landing page. Visit /api for useful functions</h4>
+  <h2>Monzo SMS</h2>
+  <p>Enter your details below</p>
+  <form method="post" action="api/register.php">
+    <div class="form-group">
+      <label for="usr">Name:</label>
+	<input name="name" type="text" class="form-control" placeholder="Your name" required id="usr">
+    </div>
+    <div class="form-group">
+      <label for="abc">"Your phone number and country coide ie. +44xxxxxxxx":</label>
+      <input name="phone" type="phone" class="form-control" placeholder="Phone number" required id="abc">
+    </div>
+    <div class="form-group">
+      <label for="abc">"The email you use to login to Monzo":</label>
+      <input name="email" type="email" class="form-control" placeholder="Email" required id="abc">
+    </div>
+    <div class="form-group">
+      <input name="submit" type="submit" value="Sign up and login to Monzo!" class="form-control" id="pwd">
+    </div>
+  </form>
+</div>
+
+</div>
+
+
 </body>
 </html>
