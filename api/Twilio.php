7d43e1597039bd1eb8d12939f7980f868bcd20ca
Feras Al-Hamadani <feras.al-hamadani@kcl.ac.uk>
2016-10-29 20:03:07 +0100

    Trying to fix Twilio

--- api/Twilio.php
+++ api/Twilio.php
@@ -21,15 +21,15 @@ error_reporting(E_ALL);
     // following the instructions to install it with Composer.
     require_once "../sitetools.php";
     use Twilio\Rest\Client;
-    
-    // Step 2: set our AccountSid and AuthToken from https://twilio.com/console
-    $AccountSid = "AC149806ac8c5e9869fd195c988df336ac";
-    $AuthToken = "15f34be56dc28327a8a62cda38459bfe";
 
-    // Step 3: instantiate a new Twilio Rest Client
-    $client = new Client($AccountSid, $AuthToken);
-    var_dump($client);
     function sendMessage($number, $message) {
+        // Step 2: set our AccountSid and AuthToken from https://twilio.com/console
+        $AccountSid = "AC149806ac8c5e9869fd195c988df336ac";
+        $AuthToken = "15f34be56dc28327a8a62cda38459bfe";
+
+        // Step 3: instantiate a new Twilio Rest Client
+        $client = new Client($AccountSid, $AuthToken);
+        
         $sms = $client->account->messages->create(
 
             // the number we are sending to - Any phone number
