e27d92dc8cb09d9f228fe2ec46c85d13d70ab607
Hugh <admin@crablab.co.uk>
2016-10-29 18:25:42 +0100

    Fixed

--- api/register.php
+++ api/register.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require("sitetools.php");
+require("../sitetools.php");
 
 $_POST = array_map("htmlspecialchars", $_POST);
 
