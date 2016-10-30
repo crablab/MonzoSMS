e457566f2532a3b329bf613e947d59bd642d6b2c
Feras Al-Hamadani <feras.al-hamadani@kcl.ac.uk>
2016-10-30 01:47:41 +0100

    Responder changes again again

--- api/responder.php
+++ api/responder.php
@@ -39,7 +39,6 @@ if($_REQUEST['Body'] == "balance") {
 }
 
  header("content-type: text/xml");
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
 	<Message><?php echo $reply ?></Message>
