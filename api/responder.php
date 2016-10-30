cdbfabc6070262cd0fb8ef8a98d47997c15ef3d4
Feras Al-Hamadani <feras.al-hamadani@kcl.ac.uk>
2016-10-30 10:56:16 +0100

    fix error

--- api/responder.php
+++ api/responder.php
@@ -11,38 +11,46 @@ use Twilio\Rest\Client;
 $user = $db->prepare("SELECT * FROM `Users` WHERE `Phone_Number` = :num");
 $user->bindParam(":num", $_POST['phone']);
 $user->execute();
+$rows = $user->rowCount();
 $data = $user->fetch(PDO::FETCH_ASSOC);
 $id = $data['monzo_id'];
 $token = $data['Authentication_Token'];
 
-// Get cURL resource
-		$curl = curl_init();
-		// Set some options - we are passing in a useragent too here
-		curl_setopt_array($curl, array(
-		    CURLOPT_RETURNTRANSFER => 1,
-		    CURLOPT_URL => 'https://api.monzo.com/balance',
-		    CURLOPT_POST => 1,
-		    CURLOPT_POSTFIELDS => array(
-		        'account_id' => $id,
-		    )
-		));
-		curl_setopt($curl,CURLOPT_HTTPHEADER, ['Authorization: Bearer ' . $token]);
+//if number exists
+if($rowCount != 0){
+	// Get cURL resource
+	$curl = curl_init();
+	// Set some options - we are passing in a useragent too here
+	curl_setopt_array($curl, array(
+	    CURLOPT_RETURNTRANSFER => 1,
+	    CURLOPT_URL => 'https://api.monzo.com/balance',
+	    CURLOPT_POST => 1,
+	    CURLOPT_POSTFIELDS => array(
+	        'account_id' => $id,
+	    )
+	));
+	curl_setopt($curl,CURLOPT_HTTPHEADER, ['Authorization: Bearer ' . $token]);
 
-$array = json_decode(curl_exec($curl), true);
-$body = $_REQUEST['Body'];
-if (stripos($body, 'balance') !== false) {
-	$balance = $array['currency'];
-	$reply = "Your balance is 45.14 GBP"; 
-} elseif(stripos($body, 'today') !== false) {
-	$today =  abs($array['spend_today'])/100 . $array['currency'];
-	$reply = "You spent 0.10 GBP today";
+	$array = json_decode(curl_exec($curl), true);
+	$body = $_REQUEST['Body'];
+	if (stripos($body, 'balance') !== false) {
+		$balance = $array['currency'];
+		$reply = "Your balance is 45.14 GBP"; 
+	} elseif(stripos($body, 'today') !== false) {
+		$today =  abs($array['spend_today'])/100 . $array['currency'];
+		$reply = "You spent 0.10 GBP today";
+	} else {
+		$reply = "Sorry I may not be able to help with that";
+	}
+
+	//header("content-type: text/xml");
+
+	 
 } else {
-	$reply = "Sorry I may not be able to help with that";
+	 $reply = "Sorry, that number has not been authorised.";
 }
 
-//header("content-type: text/xml");
-
- echo "<Response>
-	<Message>$reply</Message>
-</Response>";
+echo "<Response>
+		<Message>$reply</Message>
+	</Response>";
 ?>
